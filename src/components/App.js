092e395741e477116c29372c9d3b1ea6591d2d7e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-17 10:28:27 -0700

    Add react router to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
 import GiphSearch from './GiphSearch';
 
 class App extends Component {
   render() {
     return (
-      <GiphSearch />
+      <Router>
+        <Switch>
+          <Route path="/" component={GiphSearch}/>
+        </Switch>
+      </Router>
     );
   }
 }
 
 export default App;
-
-/*
-Component heirarchy
-
-Giph search
-
-*/
