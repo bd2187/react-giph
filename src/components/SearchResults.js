de7680194671797c5d83084da3c825b03598192c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-17 10:26:22 -0700

    Display images to UI

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
+function Giph({giph}) {
+  const { embed_url } = giph
+  return (
+    <li>
+      <a href={embed_url}>
+        <img src={giph.images.fixed_height.url} alt=""/>
+      </a>
+    </li>
+  );
+}
+
 function SearchResults ({giphs, error, isFetching}) {
-  console.log(error === "stuff")
   return (
     <div className="searchResults">
       { isFetching && "Loading" }
 
       { error === ''
         ? <ul>
-            {console.log(giphs)}
+            {giphs.map(function(giph){
+              return <Giph giph={giph} key={giph.id}/>
+            })}
           </ul>
-        : <h1>error</h1>
+        : <h1>{error}</h1>
       }
     </div>
   )
